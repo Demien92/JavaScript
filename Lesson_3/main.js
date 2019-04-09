9c842d90c7e0a9d167dfccfdebc927d15c85db3c
demien92 <demien92@yandex.ru>
2019-04-09 21:40:26 +0300

    lesson_3

---
+++ Lesson_3/main.js
@@ -0,0 +1,79 @@
+"use strict";
+
+// var x = 3;
+// switch(x) {
+//     case 1:
+//         console.log('верно');
+//         break;
+//     case 2:
+//         console.log('неверно');
+//         break;
+//     case 3:
+//         console.log('неверно');
+//         break;
+//     default:
+//         console.log('по умолчанию');
+//         break;
+// }
+
+/////////////////////////
+
+
+// var y = 'собака';
+// switch(y) {
+//     case 'кот':
+//         console.log('верно');
+//         break;
+//     case 'хомяк':
+//         console.log('неверно');
+//         break;
+//     case 'собака':
+//         console.log('неверно');
+//         break;
+//     default:
+//         console.log('по умолчанию');
+//         break;
+// }
+///////////////////////////////////////
+
+// var month = 4;
+// switch(month) {
+//     case 12 :
+//     case 1 :
+//     case 2 :
+//         console.log('зима');
+//         break;
+//     case 3 :
+//     case 4 :
+//     case 5 :
+//         console.log('весна');
+//         break;
+//     case 6 :
+//     case 7 :
+//     case 8 :
+//         console.log('лето');
+//         break;
+//     case 9 :
+//     case 10 :
+//     case 11 :
+//         console.log('осень');
+//         break;
+// }
+
+
+///////////////////////////////////
+//
+// var a = 1;
+// var b= 0;
+//
+// switch (a) {
+//     case  b +1 :
+//         console.log(1);
+//         break;
+//     default:
+//         console.log('other') ;
+// }
+
+///////////////////////////////////
+
+
---
+++ Lesson_3/main_2.js
@@ -0,0 +1,117 @@
+"use strict";
+//
+// var message = ''; /////глобальная переменная
+// function showMassage() {
+//     message = 'hi world'; //////////локальная
+//     console.log('message');
+//
+// }
+// showMassage();
+//
+//
+//
+// var userName = 'Вася';
+//
+// function showMassage2() {
+//     userName = 'Петя';
+//     var message = 'Привет, я ' + userName;
+//     console.log(message);
+//
+// }
+//
+// showMassage2();
+// alert( userName );
+//
+//
+// function showMassage3(from, text) {
+//     from = '**' + from + '**';
+//     console.log(from + ':' + text);
+//
+// }
+// var from = 'миша';
+// showMassage3(from, 'привет');
+//
+// console.log(from);
+//
+//
+//
+// function showMassage3(from, text) {
+//     if (text === undefined){
+//         text = ' текст не введен'
+//     }
+//     from = '**' + from + '**';
+//     console.log(from + ':' + text);
+//
+// }
+// var from = 'миша';
+// showMassage3(from, 'привет');
+//
+// console.log(from);
+//
+// function calculate( num1, num2) {
+//     if (num1 === undefined){
+//         num1 = 1;
+//     }
+//
+//     // if (num2 === undefined){
+//     //     num2 = 1;
+//     // }
+//     num2 = num2 || 'текст не передан';
+//
+//
+//     var a = num1 * num2;
+//     alert(a);
+// }
+// calculate(10, 20);
+//
+///////////////////////////////////
+// function calcD( a, b, c) {
+//     if(b>3){
+//         return b+b-4+a*c;a
+//     }else {
+//         return b+b+4+a*c;
+//     }
+// }
+// var text = calcD(-4, 2, 1);
+// console.log(text);
+//
+// var funcCalc = calcD;
+//
+// console.log(funcCalc);
+//
+// var text2 = funcCalc(+4, 2, 1);
+// console.log(text2);
+////////////////////////////////////
+
+
+// var calcD2 =function ( a, b, c) {   //////////////  функциональное выражение
+//     if(b>3){
+//         return;
+//     }else {
+//         return b+b+4+a*c;
+//     }
+// };
+
+
+/////////////////////////////
+
+// sayHi ('Вася');
+// function sayHi(name) {
+//     console.log(name)
+// }
+
+
+////////-------------
+
+// sayHi ('Вася');
+// var sayHi = function (name) {
+//     console.log(name)
+// };
+
+
+////-------------
+
+
+
+
+
